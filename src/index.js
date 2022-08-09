29983e33881761228a65948f751fa8045663a488
Mayis <saroyan.mayis.ait@gmail.com>
2022-08-09 19:38:24 +0400

    slider finish

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Slider from "./Slider.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Slider />);
+root.render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
